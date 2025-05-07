257d0aae81b8e229e9d65f6f64c7623475fc48df
lzadhito <lzadhito@gmail.com>
2025-05-07 22:54:03 +0700

    feat: add tailwind, and changed to lzadhito.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lzadhito.github.io',
-	integrations: [mdx(), sitemap()],
+  site: 'https://lzadhito.github.io',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
