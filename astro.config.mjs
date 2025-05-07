564727bbf1794500a788967bb499451145745236
lzadhito <lzadhito@gmail.com>
2025-05-07 22:26:57 +0700

    feat: add astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lzadhito.github.io',
+	base: 'lzadhito',
 	integrations: [mdx(), sitemap()],
 });
